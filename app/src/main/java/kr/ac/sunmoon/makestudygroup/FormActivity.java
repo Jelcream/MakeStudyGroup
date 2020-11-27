f2d95d774952d6991e10b69f125a81371ba3a752
KimGwangYong <brightdragom@gmail.com>
2020-11-27 13:57:22 +0900

    merge

--- app/src/main/java/kr/ac/sunmoon/makestudygroup/FormActivity.java
+++ app/src/main/java/kr/ac/sunmoon/makestudygroup/FormActivity.java
@@ -1,7 +1,10 @@
 package kr.ac.sunmoon.makestudygroup;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -10,10 +13,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.StorageReference;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -26,6 +32,20 @@ public class FormActivity extends Activity {
     String editor;
     String dateStr;
     String editorUid;
+
+
+    private Uri mDownloadImageUri; //프로필사진 스토리지 URI
+    private String mTmpDownloadImageUri; //Shared에서 받아올떄 String형이라 임시로 받아오는데 사용
+    private Bitmap img; //비트맵 프로필사진 (이걸.
+    private StorageReference mStorageRef; //파이어베이스 스토리지
+    private StorageReference mProfileRef; //프로필이미지 담을 파베 스토리
+
+    //RequestCode
+    final static int PICK_IMAGE = 1; //갤러리에서 사진선택
+    final static int CAPTURE_IMAGE = 2;  //카메라로찍은 사진선택
+    private String mCurrentPhotoPath; //카메라로 찍은 사진 저장할 루트경로\
+    FirebaseFirestore db; //파이어스토어 디비
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,5 +84,38 @@ public class FormActivity extends Activity {
         DBref.child(editorUid+"-"+dateStr).setValue(hashmap);
         //데이터베이스에 등록
         finish();
+    }
+
+    public void addImg(View v){
+
+        photoDialogRadio();
+
+        finish();
+    }
+    //사진찍기 or 앨범에서 가져오기 선택 다이얼로그
+    private void photoDialogRadio() {
+        final CharSequence[] PhotoModels = {"갤러리에서 가져오기", "카메라로 촬영 후 가져오기", "기본사진으로 하기"};
+        AlertDialog.Builder alt_bld = new AlertDialog.Builder(this);
+        //alt_bld.setIcon(R.drawable.icon);
+        alt_bld.setTitle("배경사진 설정");
+        alt_bld.setSingleChoiceItems(PhotoModels, -1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                Toast.makeText(FormActivity.this, PhotoModels[item] + "가 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                if (item == 0) { //갤러리
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(intent, PICK_IMAGE);
+                } else if (item == 1) { //카메라찍은 사진가져오기
+                    //takePictureFromCameraIntent();
+                } else { //기본화면으로하기
+                    //mPhotoCircleImageView.setImageResource(R.drawable.profile);
+                    img = null;
+                    mTmpDownloadImageUri = null;
+                }
+            }
+        });
+        AlertDialog alert = alt_bld.create();
+        alert.show();
     }
 }
